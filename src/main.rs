111d3634d868916116038af6a567df032d817355
Laryssa Ferreira <laryssa.oliveira1902@gmail.com>
2025-01-30 12:28:25 -0300

    ⚙️ feat: Insertion of Rocket dependencies

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use rocket::{get, routes, launch};
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
+}
