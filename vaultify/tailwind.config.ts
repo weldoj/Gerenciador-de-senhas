a3a12d7ba9ed091996a7a35f36017e093c4cebe2
victorcosta <victorhenriquecosta23@gmail.com>
2025-01-31 23:36:38 -0300

    Feat/ Novas telas adicionadas ao projeto (2fa , adicionar-senha , cadastro , redefinir-senha)

--- vaultify/tailwind.config.ts
+++ vaultify/tailwind.config.ts
@@ -8,6 +8,9 @@ export default {
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
       },
+      colors: {
+        background: "#403B4D",
+      },
     },
   },
   plugins: [],
